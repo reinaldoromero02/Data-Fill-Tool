73fb2d4368d38fc91c545b4e09cf35aab14d3ed5
reinaldoromero02 <reinado.barbeiro@gmail.com>
2026-08-01 12:24:02 +0000

    primeiro commit

--- artifacts/programacao-entrega/dev-dist/sw.js
+++ artifacts/programacao-entrega/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-970124e6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/",
-    "revision": "0.qedd2hrbkbk"
+    "revision": "0.cd416bapd0s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
